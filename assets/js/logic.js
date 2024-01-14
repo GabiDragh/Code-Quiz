3eca19b21b9e4ea78bb9acdd9cd88465055829db
Gabi Draghici <draghici.gabi89@gmail.com>
2024-01-14 18:51:18 +0000

    Questions.js file choices property name replaced with answers. Logic.js file ordered list items structure added and event listeners attached to each items.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,4 @@
-//console.log(questions);
+
 var body = document.querySelector("body");
 var h1 = document.querySelector("h1");
 var startScreen = document.querySelector("#start-screen");
@@ -7,8 +7,10 @@ var timerElement = document.querySelector(".timer");
 var questionsElement = document.querySelector("#questions");
 var questionTitleElement = document.querySelector("#question-title");
 var choicesElement = document.querySelector("#choices");
+var wrapper = document.querySelector(".wrapper");
 
 //Insert pub quiz image under start button, inside the start-screen div
+
 //Create image element within the start-screen div
 var image = document.createElement("img");
 //Set content and style
@@ -22,6 +24,44 @@ body.setAttribute("style", "font-family: emoji");
 startButton.setAttribute("style", "background-color: navy; color: orange; size: 110%");
 questionTitleElement.setAttribute("style", "text-align: center");
 
+//Create answers ordered list
+
+var choicesList = document.createElement("ol");
+
+//Create ordered list items
+
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+
+//Add text for list items
+
+li1.textContent = "";
+li2.textcontent = "";
+li3.textcontent = "";
+
+//Add event listeners to the list items
+
+li1.addEventListener ("click", function () {
+    console.log("Item 1 is clicked")
+})
+li2.addEventListener ("click", function () {
+    console.log("Item 2 is clicked")
+})
+li3.addEventListener ("click", function () {
+    console.log("Item 3 is clicked")
+})
+
+//Append ordered list
+
+choicesElement.appendChild(choicesList);
+
+//Append items to the ordered list
+
+choicesList.appendChild(li1);
+choicesList.appendChild(li2);
+choicesList.appendChild(li3);
+
 
 var isWin = false;
 var wrongAnswer = false;
@@ -29,9 +69,9 @@ var timerInterval;
 var timerCount = 0;
 var selectedQuestion = "";
 var randomQuestion = "";
-var randomQuestion = "";
-var firstQuestionDisplay = "";
 var choicesList;
+
+//1. When I click the start button (ADD EVENT LISTENER -CLICK), I want the timer to start (SET TIMER FUNCTION) and the first question to appear (PROMPT/DISPLAY????)
 
 //Add the start button event
 function startGame() {
@@ -69,21 +109,41 @@ function askFirstQuestion() {
     //Replace start screen div with question div
     startScreen.style.display = "none";
     questionsElement.style.display = "block";
+    
+    //Show answers ordered list items
 
-    //Create ordered list for the answers
-    //Create clickable buttons 
-    //if answers right->next question
-    //If answer wrong->display wrong and take 5 seconds off timer
+    li1.innerHTML = "";
+    //for ( var i = 0; i < quizQuestions[1].length; i++) {
+    //     var answer = li1.textContent 
+    //     answer = questionQuestions[1][i];
+    //     console.log(answer);
+    //     answer.style.display = "block";
+    // }
+    
+   
     //Display ordered list
+    choicesList.style.display = "block";
+    //Create clickable buttons 
+    
+    //if answers right->next question
+
+    //If answer wrong->display wrong and take 5 seconds off timer
+
+    
 
 };
 
+//for (var obj of quizQuestions) {
+    //console.log(obj.answers);
+//}
 
+//var propArr = quizQuestions.map(obj => obj.answers);
+//console.log(propArr);
 //function nextQuestion() {
 
 //}
 startButton.addEventListener("click", startGame);
-//When I click the start button (ADD EVENT LISTENER -CLICK), I want the timer to start (SET TIMER FUNCTION) and the first question to appear (PROMPT/DISPLAY????)
+
 //Each question has a clickable button (ADD EVENT LISTENER-CLICK ON VARIABLE PROPERTY)
 //When the answer in selected, next question appears (BONUS: select a random question once pressed next question?)
 //In case of an incorrect answer, one second gets subtracted from the remaining time
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,7 +5,7 @@ var quizQuestions = [
     {
       //questionId: 1, //might not need? Adding as it might be useful for random selection of questions
       questionTitle: "In 1992, the first text message sent to a phone said what?", 
-      choices: [
+      answers: [
         { text: "Merry Christmas", isCorrect: true },
         { text: "Happy Birthday", isCorrect: false },
         { text: "Ur dumped", isCorrect: false },
@@ -15,7 +15,7 @@ var quizQuestions = [
     {
         //questionId: 2, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "The sound of the police siren changing pitch when it moves towards and away from you is know as what?",
-        choices: [
+        answers: [
           { text: "Doppler effect", isCorrect: true },
           { text: "Hertz effect", isCorrect: false },
           { text: "Ohms effect", isCorrect: false },
@@ -25,7 +25,7 @@ var quizQuestions = [
     {
         //questionId: 3, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Who is the chef credited with the invention of potato crisps in 1853?", 
-        choices: [
+        answers: [
           { text: "CB Fry", isCorrect: false },
           { text: "Titus Salt", isCorrect: false },
           { text: "George Crum", isCorrect: true },
@@ -35,7 +35,7 @@ var quizQuestions = [
     {
         //questionId: 4, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "A pet feline called Tardar Sauce became an internet sensation under what name?", 
-        choices: [
+        answers: [
           { text: "Chairman Miaow", isCorrect: false },
           { text: "Grumpy Cat", isCorrect: true },
           { text: "Santa Paws", isCorrect: false },
@@ -45,7 +45,7 @@ var quizQuestions = [
     {
         //questionId: 5, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which Carribean country is regarded as the home of the steel band?", 
-        choices: [
+        answers: [
           { text: "Jamaica", isCorrect: false },
           { text: "Trindad and Tobago", isCorrect: true },
           { text: "Barbados", isCorrect: false },
@@ -55,7 +55,7 @@ var quizQuestions = [
     {
         //questionId: 6, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "How many legs does a lobster have?", 
-        choices: [
+        answers: [
           { text: "6", isCorrect: false },
           { text: "8", isCorrect: false },
           { text: "10", isCorrect: true },
@@ -65,7 +65,7 @@ var quizQuestions = [
     {
         //questionId: 7, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "In which sport might you perform 'randolphs' and 'rudolphs'?", 
-        choices: [
+        answers: [
           { text: "Ice-skating", isCorrect: false },
           { text: "Ski-jumping", isCorrect: false },
           { text: "Trampolining", isCorrect: true },
@@ -75,7 +75,7 @@ var quizQuestions = [
     {
         //questionId: 8, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "On a QWERTY keyboard, which of these consecutive letters are NOT placed side by side?", 
-        choices: [
+        answers: [
           { text: "ABC", isCorrect: true },
           { text: "SFGH", isCorrect: false },
           { text: "JKL", isCorrect: false },
@@ -85,7 +85,7 @@ var quizQuestions = [
     {
         //questionId: 9, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "In karaoke and karate, what does the Japanese word kara mean?", 
-        choices: [
+        answers: [
           { text: "Empty", isCorrect: true },
           { text: "Strong", isCorrect: false },
           { text: "Shaking", isCorrect: false },
@@ -95,7 +95,7 @@ var quizQuestions = [
     {
         //questionId: 10, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which US president has a middle initial that didn't stand for anything?", 
-        choices: [
+        answers: [
           { text: "Lyndon B Johnson", isCorrect: false },
           { text: "George W Bush", isCorrect: false },
           { text: "Harry S Truman", isCorrect: true },
@@ -105,7 +105,7 @@ var quizQuestions = [
     {
         //questionId: 11, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which of these is a real medical condition?", 
-        choices: [
+        answers: [
           { text: "Geographic tongue", isCorrect: true },
           { text: "Historic gum", isCorrect: false },
           { text: "Religious tooth", isCorrect: false },
@@ -115,7 +115,7 @@ var quizQuestions = [
     {
         //questionId: 12, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "'Modern life is rubbish' was the second album by which band?", 
-        choices: [
+        answers: [
           { text: "Oasis", isCorrect: false },
           { text: "Stereophonics", isCorrect: false },
           { text: "Blur", isCorrect: true },
@@ -125,7 +125,7 @@ var quizQuestions = [
     {
         //questionId: 13, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which of these is a genuine language setting on Facebook?", 
-        choices: [
+        answers: [
           { text: "Double Dutch", isCorrect: false },
           { text: "Pirate English", isCorrect: true },
           { text: "Dog Latin", isCorrect: false },
@@ -135,7 +135,7 @@ var quizQuestions = [
     {
         //questionId: 14, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "If a french person says 'Je mange', what are they doing?", 
-        choices: [
+        answers: [
           { text: "Eating", isCorrect: true },
           { text: "Crying", isCorrect: false },
           { text: "Walking", isCorrect: false },
@@ -145,7 +145,7 @@ var quizQuestions = [
     {
         //questionId: 15, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "How long did Apollo 11 take to travel to the Moon?", 
-        choices: [
+        answers: [
           { text: "18 hours", isCorrect: false },
           { text: "4 days", isCorrect: true },
           { text: "2 weeks", isCorrect: false },
@@ -155,7 +155,7 @@ var quizQuestions = [
     {
         //questionId: 16, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Henri Becquerel won a shared Nobel Prize for accidentally discovering what?", 
-        choices: [
+        answers: [
           { text: "X-rays", isCorrect: false },
           { text: "Magnetism", isCorrect: false },
           { text: "Radioactivity", isCorrect: true },
@@ -165,7 +165,7 @@ var quizQuestions = [
     {
         //questionId: 17, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which of these is a Welsh slang term for a microwave oven?", 
-        choices: [
+        answers: [
           { text: "Hotty-hoo-hoo", isCorrect: false },
           { text: "Beepy-spin", isCorrect: false },
           { text: "Popty-ping", isCorrect: true },
@@ -175,7 +175,7 @@ var quizQuestions = [
     {
         //questionId: 18, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "'Master of the House' and 'Bring Him Home' are songs from which musical?'", 
-        choices: [
+        answers: [
           { text: "Sweeney Todd", isCorrect: false },
           { text: "Miss Saigon", isCorrect: false },
           { text: "Les Miserables", isCorrect: true },
@@ -185,7 +185,7 @@ var quizQuestions = [
     {
         //questionId: 19, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which of these world championships, held in Finland, is the oldest?", 
-        choices: [
+        answers: [
           { text: "Air guitar", isCorrect: false },
           { text: "Wife carrying", isCorrect: true },
           { text: "Mobile phone throwing", isCorrect: false },
@@ -195,7 +195,7 @@ var quizQuestions = [
     {
         //questionId: 20, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which of these French foods is a soup?", 
-        choices: [
+        answers: [
           { text: "Boudin noir", isCorrect: false },
           { text: "Bouillon", isCorrect: true },
           { text: "Boeuf bourguignon", isCorrect: false },
@@ -205,7 +205,7 @@ var quizQuestions = [
     {
         //questionId: 21, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "How would you travel if you were in a 'felucca'?", 
-        choices: [
+        answers: [
           { text: "Fly in it", isCorrect: false },
           { text: "Pedal it", isCorrect: false },
           { text: "sail in it", isCorrect: true },
@@ -215,7 +215,7 @@ var quizQuestions = [
     {
         //questionId: 22, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "What type of computer was the first laptop computer?", 
-        choices: [
+        answers: [
           { text: "Apple Macintosh", isCorrect: false },
           { text: "IBM", isCorrect: false },
           { text: "Osborne 1", isCorrect: true },
@@ -225,7 +225,7 @@ var quizQuestions = [
     {
         //questionId: 23, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "In March 2013, Belgium released stamps with which flavour glue?", 
-        choices: [
+        answers: [
           { text: "Beer", isCorrect: false },
           { text: "Chocolate", isCorrect: true },
           { text: "Mussels", isCorrect: false },
@@ -235,7 +235,7 @@ var quizQuestions = [
     {
         //questionId: 24, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "The Clarence Hotel in Ireland is co-owned by the members of which band?", 
-        choices: [
+        answers: [
           { text: "Boyzone", isCorrect: false },
           { text: "Gun'n'Roses", isCorrect: false },
           { text: "U2", isCorrect: true },
@@ -245,7 +245,7 @@ var quizQuestions = [
     {
         //questionId: 25, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "What is the sum of numbers from one to nine?", 
-        choices: [
+        answers: [
           { text: "45", isCorrect: true },
           { text: "39", isCorrect: false },
           { text: "34", isCorrect: false },
@@ -255,7 +255,7 @@ var quizQuestions = [
     {
         //questionId: 26, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "A 'Fat Rascal' is a type of what?",
-        choices: [
+        answers: [
           { text: "Cat", isCorrect: false },
           { text: "Cactus", isCorrect: false },
           { text: "Cake", isCorrect: true },
@@ -265,7 +265,7 @@ var quizQuestions = [
     {
         //questionId: 27, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "In 'The Sound of Music', which character is described as a 'flibbertigibbet, a will-o'-the-wisp, a clown'?", 
-        choices: [
+        answers: [
           { text: "Maria", isCorrect: true },
           { text: "Captain Von Trapp", isCorrect: false },
           { text: "Liesl", isCorrect: false },
@@ -275,7 +275,7 @@ var quizQuestions = [
     {
         //questionId: 28, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "On the standard London Monopoly board. which of these properties has the highest value?", 
-        choices: [
+        answers: [
           { text: "Trafalgar Square", isCorrect: false },
           { text: "Oxford Street", isCorrect: true },
           { text: "Bond Street", isCorrect: false },
@@ -285,7 +285,7 @@ var quizQuestions = [
     {
         //questionId: 29, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which of the Beatles is barefoot on the front cover of the album 'Abbey Road'?", 
-        choices: [
+        answers: [
           { text: "George Harrison", isCorrect: false },
           { text: "Paul McCartney", isCorrect: true },
           { text: "Ringo Starr", isCorrect: false },
@@ -295,7 +295,7 @@ var quizQuestions = [
     {
         //questionId: 30, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Epernay in northern France calls itself the capital of which drink?", 
-        choices: [
+        answers: [
           { text: "Calvados", isCorrect: false },
           { text: "Champagne", isCorrect: true },
           { text: "Cognac", isCorrect: false },
@@ -305,7 +305,7 @@ var quizQuestions = [
     {
         //questionId: 31, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which of these is the name of a mountain in the US state of Washington?", 
-        choices: [
+        answers: [
           { text: "Bourbon Tom", isCorrect: false },
           { text: "Whiskey Dick", isCorrect: true },
           { text: "Rum Harry", isCorrect: false },
@@ -315,7 +315,7 @@ var quizQuestions = [
     {
         //questionId: 32, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "The two promotories at the eastern end of the Strait of Gibraltar are called the 'Pillars of ... ' what?", 
-        choices: [
+        answers: [
           { text: "Hercules", isCorrect: true },
           { text: "Zeus", isCorrect: false },
           { text: "Poseidon", isCorrect: false },
@@ -325,7 +325,7 @@ var quizQuestions = [
     {
         //questionId: 33, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "The tourist attraction Wookey Hole is famous for which natural features?", 
-        choices: [
+        answers: [
           { text: "Caves", isCorrect: true },
           { text: "Moving stones", isCorrect: false },
           { text: "Natural spas", isCorrect: false },
@@ -335,7 +335,7 @@ var quizQuestions = [
     {
         //questionId: 34, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "What name is given to the area of the USA that suffered hardship as a result of drought and windstorms in the 1930s?", 
-        choices: [
+        answers: [
           { text: "Dust bowl", isCorrect: true },
           { text: "Dirt basin", isCorrect: false },
           { text: "Soil dish", isCorrect: false },
@@ -345,7 +345,7 @@ var quizQuestions = [
     {
         //questionId: 35, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "At the annual World Biscuit Throwing Championship, which type of biscuit is used?",
-        choices: [
+        answers: [
           { text: "Digestive", isCorrect: false },
           { text: "Rich Tea", isCorrect: true },
           { text: "Custard Cream", isCorrect: false },
@@ -355,7 +355,7 @@ var quizQuestions = [
     {
         //questionId: 36, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which of these birds is NOT mentioned in the song 'The Twelve Days of Christmas'?", 
-        choices: [
+        answers: [
           { text: "Geese", isCorrect: false },
           { text: "Swans", isCorrect: false },
           { text: "Swallows", isCorrect: true },
@@ -365,7 +365,7 @@ var quizQuestions = [
     {
         //questionId: 37, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "What type of geographical feature is a 'gulch'?", 
-        choices: [
+        answers: [
           { text: "Deep ravine", isCorrect: true },
           { text: "Hill of a baked mud", isCorrect: false },
           { text: "Stagnant pond", isCorrect: false },
@@ -375,7 +375,7 @@ var quizQuestions = [
     {
         //questionId: 38, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "What is the meaning of 'Hakuna Matata'?", 
-        choices: [
+        answers: [
           { text: "No worries", isCorrect: true },
           { text: "Good night", isCorrect: false },
           { text: "Thank you", isCorrect: false },
@@ -385,7 +385,7 @@ var quizQuestions = [
     {
         //questionId: 39, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Which instrument is nicknamed 'the clown of the orchestra'?", 
-        choices: [
+        answers: [
           { text: "Bassoon", isCorrect: true },
           { text: "Tuba", isCorrect: false },
           { text: "Piccolo", isCorrect: false },
@@ -395,7 +395,7 @@ var quizQuestions = [
     {
         //questionId: 40, //might not need? Adding as it might be useful for random selection of questions
         questionTitle: "Equipment for which activity is stored in a tack room?", 
-        choices: [
+        answers: [
           { text: "Rowing", isCorrect: false },
           { text: "Photography", isCorrect: false },
           { text: "Riding", isCorrect: true },
@@ -440,8 +440,8 @@ if (questionTitles.length === uniquequestionTitles.size) {
 //      console.log(questions[i].length);
 //   }
 
-   for (var j = 0; j < quizQuestions[0].choices[1].length; j++) {
-      if (quizQuestions[i].choices[1][j].isCorrect === true) {
+   for (var j = 0; j < quizQuestions[0].answers[1].length; j++) {
+      if (quizQuestions[i].answers[1][j].isCorrect === true) {
           trueCount++;
       } else {
           falseCount++
